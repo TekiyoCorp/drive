64071c39e8124e2721e23316e2c9513c01aef4d0
Naseem <devnaseemkhan@gmail.com>
2025-09-25 03:27:09 +0500

    Update font settings: Define font families for paragraphs and body using CSS variables

--- constants/fonts.ts
+++ constants/fonts.ts
@@ -3,9 +3,13 @@ import { Inter } from "next/font/google";
 export const heading = Inter({
   subsets: ["latin"],
   variable: "--font-heading",
+  display: "swap",
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 export const base = Inter({
   subsets: ["latin"],
   variable: "--font-base",
+  display: "swap",
+  weight: ["300", "400", "500", "600"],
 });
