34eefda9cd00fd89d26b4cc1e9cd7c82ba5b11fc
Naseem <devnaseemkhan@gmail.com>
2025-09-25 14:27:55 +0500

    Add local font integration: Replace Google Fonts with local InterDisplay fonts and update CSS variables for font families

--- constants/fonts.ts
+++ constants/fonts.ts
@@ -1,15 +1,65 @@
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-export const heading = Inter({
-  subsets: ["latin"],
+export const heading = localFont({
+  src: [
+    {
+      path: "../public/fonts/InterDisplay-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/InterDisplay-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/InterDisplay-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/InterDisplay-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/InterDisplay-Black.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/InterDisplay-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
   variable: "--font-heading",
   display: "swap",
-  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
-export const base = Inter({
-  subsets: ["latin"],
+export const base = localFont({
+  src: [
+    {
+      path: "../public/fonts/InterDisplay-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/InterDisplay-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/InterDisplay-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/InterDisplay-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+  ],
   variable: "--font-base",
   display: "swap",
-  weight: ["300", "400", "500", "600"],
 });
