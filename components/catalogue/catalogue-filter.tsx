9ed2bd97233ddbafe16d4176dd7feb77c7c9ddc7
Naseem <devnaseemkhan@gmail.com>
2025-10-24 18:49:21 +0500

    feat: Update CatalogueFilter component for improved responsiveness and styling

--- components/catalogue/catalogue-filter.tsx
+++ components/catalogue/catalogue-filter.tsx
@@ -25,28 +25,28 @@ const CatalogueFilter = () => {
       <Container animation="fadeRight" delay={0.1} className="w-full">
         <LiquidGlass
           className={cn(
-            "border border-white/50 border-r-0 border-l-0 rounded-[40px] !w-full !items-start flex-col md:flex-row transition-all duration-500",
-            showFilters ? "!h-96" : "max-md:!h-12 !h-fit"
+            "border border-white/50 border-r-0 border-l-0 rounded-[40px] !w-full !items-start flex-col lg:flex-row transition-all duration-500",
+            showFilters ? "!h-96 lg:!h-12" : "max-lg:!h-12 !h-fit"
           )}
         >
           <div
             onClick={() => setShowFilters(!showFilters)}
-            className="flex flex-row items-center justify-center gap-4 text-sm font-medium px-8 py-4 w-full md:hidden cursor-pointer"
+            className="flex flex-row items-center justify-center gap-4 text-sm font-medium px-8 py-4 w-full lg:hidden cursor-pointer"
           >
             Voir les filtres
             <SlidersHorizontal className="size-5" />
           </div>
           <div
             className={cn(
-              "flex flex-col md:flex-row items-center justify-between gap-4 md:gap-6 xl:gap-10 text-sm font-medium px-8 w-full transition-all duration-500",
-              showFilters ? "h-80 py-4" : "h-0 py-0 md:!h-12"
+              "flex flex-col lg:flex-row items-center lg:justify-between justify-center gap-4 lg:gap-0 text-sm font-medium px-8 w-full transition-all duration-500",
+              showFilters ? "h-80 py-4 lg:h-fit" : "h-0 py-0 lg:!h-12 opacity-0 lg:opacity-100"
             )}
           >
             {filters.map((filter, index) => (
               <div
                 key={filter.id}
                 className={cn(
-                  "text-sm font-medium transition-all duration-200 whitespace-nowrap md:border-r border-white/40 w-full",
+                  "text-sm font-medium transition-all duration-200 whitespace-nowrap lg:border-r border-white/40 flex-1 flex items-center justify-center",
                   index === filters.length - 1 ? "!border-r-0" : ""
                 )}
               >
@@ -54,7 +54,7 @@ const CatalogueFilter = () => {
               </div>
             ))}
 
-            <div className="max-md:hidden">
+            <div className="max-lg:hidden">
               <SlidersHorizontal className="size-5" />
             </div>
           </div>
